a4bcb9e3342ca70c686a5cb6eeb821f0080e7267
jmarkgraf <jonas.markgraf@gmail.com>
2017-04-17 21:17:19 -0500

    Parallelization and Speed Tests

--- PS6.R
+++ PS6.R
@@ -1,7 +1,31 @@
-sg.int<-function(g,...,lower,upper, dimension = no.dim){
+##############################################################
+# Statistical Programming 
+# Problem Set 6
+# Author: Jonas Markgraf
+# Date: April 17, 2017
+
+library(testthat)
+library(microbenchmark)
+library()
+
+
+# Function that can deal with multiple dimensions and parallelize estimation
+# ===========================================================================
+
+sg.int<-function(g,...,lower,upper, parallel = F){
   require("SparseGrid")
+  require("dplyr")
+  require("parallel")
+  require("doMC")
   
-  # adjusting upper and lower bound downward
+  # identify and register number of cores for parallelization
+  cores <- detectCores()
+  registerDoMC(cores)
+  
+  # identify number of dimensions
+  no.dim <- length(upper)
+  
+  # adjusting upper and lower bound downward/upward 
   lower<-floor(lower)
   upper<-ceiling(upper)
   
@@ -10,7 +34,7 @@ sg.int<-function(g,...,lower,upper, dimension = no.dim){
   
   # function that creates matrix of all possible combinations of values between the lower 
   # and upper bound values, depending on number of dimensions
-  gridss<-as.matrix(expand.grid(lapply(function(x) seq(lower[x], upper[x]-1, by=1))))
+  gridss<-as.matrix(expand.grid(lapply(1:no.dim, function(x) seq(lower[x], upper[x]-1, by=1))))
   
   # function creates nodes and weights for integration
   sp.grid <- createIntegrationGrid( 'KPU', dimension=no.dim, k=5 )
@@ -24,7 +48,43 @@ sg.int<-function(g,...,lower,upper, dimension = no.dim){
     weights<-c(weights,sp.grid$weights)
   }
   
-  gx.sp <- apply(nodes, 1, g,...)
+  # parallelize 'apply' function
+  gx.sp <- aaply(nodes, 1, g, .parallel = parallel)
   val.sp <- gx.sp %*%weights
+  
+  # return value
   val.sp
 }
+
+
+# Measure speed for parallelized and non-parallelized function
+# =============================================================
+
+# create test functions
+test2dim <- function(x) x[1]^4 + 2*x[2]  # 2 dimensions
+  
+test3dim <- function(x) 4*x[1] + 3*x[2]^2 + 2*x[3]^3  # 3 dimensions
+
+test4dim <- function(x) 4*x[1] + 3*x[2]^2 + 2*x[3]^3 + x[4]^4  # 4 dimensions
+
+# measure speed for 2 dimensions
+microbenchmark(
+  "solo"=sg.int(g=test2dim, lower = c(0, 0), upper = c(4.2, 4.2)),
+  "parallel"=sg.int(g=test2dim, lower = c(0, 0), upper = c(4.2, 4.2), parallel = T),
+  times =20
+)
+
+# measure speed for 3 dimensions
+microbenchmark(
+  "solo"=sg.int(g=test3dim, lower = c(0, 0, 0), upper = c(4.2, 4.2, 4.2)),
+  "parallel"=sg.int(g=test3dim, lower = c(0, 0, 0), upper = c(4.2, 4.2, 4.2), parallel = T),
+  times =20
+)
+
+# measure speed for 3 dimensions
+microbenchmark(
+  "solo"=sg.int(g=test4dim, lower = c(0, 0, 0, 0), upper = c(4.2, 4.2, 4.2, 4.2)),
+  "parallel"=sg.int(g=test4dim, lower = c(0, 0, 0, 0), upper = c(4.2, 4.2, 4.2, 4.2), parallel = T),
+  times =20
+)
+
