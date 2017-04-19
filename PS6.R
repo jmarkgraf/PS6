3f7ace8d8a9b8e1fabbb83badef0ae05f98cd905
jmarkgraf <jonas.markgraf@gmail.com>
2017-04-19 15:59:30 -0500

    Add adaptIntegrate to benchmarking.

--- PS6.R
+++ PS6.R
@@ -6,13 +6,13 @@
 
 library(testthat)
 library(microbenchmark)
-library()
 
+rm(list = ls())
 
 # Function that can deal with multiple dimensions and parallelize estimation
 # ===========================================================================
 
-sg.int<-function(g,...,lower,upper, parallel = F){
+sg.int<-function(g,...,lower, upper, parallel = F){
   require("SparseGrid")
   require("dplyr")
   require("parallel")
@@ -56,35 +56,50 @@ sg.int<-function(g,...,lower,upper, parallel = F){
   val.sp
 }
 
+# Generate test functions ###########################
+# ===================================================
+
+# create test functions for unit testing and speed benchmarking
+test2dim <- function(x) 4*x[1] + 3*x[2]^2  # 2 dimensions
+
+test3dim <- function(x) 4*x[1] + 3*x[2]^2 + 2*x[3]  # 3 dimensions
+
+test4dim <- function(x) 4*x[1] + 3*x[2]^2 + 2*x[3] + x[4]  # 4 dimensions
+
+
+# Unit Testing ######################################
+# ===================================================
+
+# test that function throws error when lower and upper bound are confused
+test_that("Test that upper is larger than lower",
+  throws_error(sg.int(test2dim, upper = c(1, 1), lower = c(2, 2))))
+
+# test that function estimates correct results
+testthat("Test that function estimates correct values")
 
 # Measure speed for parallelized and non-parallelized function
 # =============================================================
 
-# create test functions
-test2dim <- function(x) x[1]^4 + 2*x[2]  # 2 dimensions
-  
-test3dim <- function(x) 4*x[1] + 3*x[2]^2 + 2*x[3]^3  # 3 dimensions
-
-test4dim <- function(x) 4*x[1] + 3*x[2]^2 + 2*x[3]^3 + x[4]^4  # 4 dimensions
-
 # measure speed for 2 dimensions
 microbenchmark(
-  "solo"=sg.int(g=test2dim, lower = c(0, 0), upper = c(4.2, 4.2)),
-  "parallel"=sg.int(g=test2dim, lower = c(0, 0), upper = c(4.2, 4.2), parallel = T),
+  "solo"=sg.int(g=test2dim, lower = c(0, 0), upper = c(4, 4)),
+  "parallel"=sg.int(g=test2dim, lower = c(0, 0), upper = c(4, 4), parallel = T),
   times =20
 )
+## function without parallelization is faster
 
 # measure speed for 3 dimensions
 microbenchmark(
-  "solo"=sg.int(g=test3dim, lower = c(0, 0, 0), upper = c(4.2, 4.2, 4.2)),
-  "parallel"=sg.int(g=test3dim, lower = c(0, 0, 0), upper = c(4.2, 4.2, 4.2), parallel = T),
-  times =20
+  "solo"=sg.int(g=test3dim, lower = c(0, 0, 0), upper = c(4, 4, 4)),
+  "parallel"=sg.int(g=test3dim, lower = c(0, 0, 0), upper = c(4, 4, 4), parallel = T),
+  times =5
 )
+## function without parallelization is faster
 
-# measure speed for 3 dimensions
+# measure speed for 4 dimensions
 microbenchmark(
-  "solo"=sg.int(g=test4dim, lower = c(0, 0, 0, 0), upper = c(4.2, 4.2, 4.2, 4.2)),
-  "parallel"=sg.int(g=test4dim, lower = c(0, 0, 0, 0), upper = c(4.2, 4.2, 4.2, 4.2), parallel = T),
-  times =20
+  "solo"=sg.int(g=test4dim, lower = c(0, 0, 0, 0), upper = c(4, 4, 4, 4)),
+  "parallel"=sg.int(g=test4dim, lower = c(0, 0, 0, 0), upper = c(4, 4, 4, 4), parallel = T),
+  times=5
 )
-
+## function without parallelization is faster
